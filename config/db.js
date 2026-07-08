015efbbd09779588c05d91d0a077abdf7a9a3296
Sahad M P <sahadmp@Sahads-Mac-mini.local>
2026-07-08 22:34:27 +0530

    update monfo

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/mydb');
+        await mongoose.connect('mongodb+srv://sahaddev25_db_user:Xf9TcGsQdD2ZvQr3@sahaddev.bugwu6p.mongodb.net/?appName=sahaddev');
         console.log('mongoDb connected');
     } catch (error) {
         console.log('DB connection error', error);
